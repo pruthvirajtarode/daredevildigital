a208b5199380efd82fcee2af42de5ee2575bd538
Pruth <pruth@bindu.dev>
2026-09-23 14:08:41 +0530

    Re-add WhatsApp widget as requested by client

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,6 +2,7 @@ import type { ReactNode } from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import ScrollToTop from '../ui/ScrollToTop';
+import FloatingWhatsApp from '../ui/FloatingWhatsApp';
 import ScrollToHash from '../ui/ScrollToHash';
 
 interface LayoutProps {
@@ -17,6 +18,7 @@ export default function Layout({ children }: LayoutProps) {
         {children}
       </main>
       <Footer />
+      <FloatingWhatsApp />
       <ScrollToTop />
     </div>
   );
